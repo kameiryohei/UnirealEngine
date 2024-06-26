060c792454c431e5a5137322e2d42d2a5f203949
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2024-06-26 15:26:42 +0900

    chore: add: Bullet

--- Assets/FireBullet.cs
+++ Assets/FireBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireBullet : MonoBehaviour {
+public class FireBullet   : MonoBehaviour {
 
     // bullet prefab
     public GameObject bullet;
